d1039601831f0582d89f9f67d98205bfe25250e0
dv0c <tasosmeidanis12@gmail.com>
2023-06-22 23:16:25 +0300

    new file:   src/app/@authModal/(.)sign-in/page.tsx 	new file:   src/app/@authModal/default.tsx 	modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
     <html
@@ -22,8 +24,9 @@ export default function RootLayout({
       className={cn("bg-white text-slate-900 antialiased", inter.className)}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        {/* @ts-expect-error server component */}
         <Navbar />
-
+        {authModal}
         <div className="container max-w-7xl mx-auto h-full pt-12">
           {children}
         </div>
