a88cd058851a332bb10ed7937f4562601b333fdb
dv0c <tasosmeidanis12@gmail.com>
2023-06-22 11:07:31 +0300

    deleted:    .env.example 	modified:   next.config.js 	new file:   package-lock.json 	modified:   prisma/schema.prisma 	new file:   src/app/(auth)/sign-in/page.tsx 	new file:   src/app/api/auth/[...nextauth]/route.ts 	modified:   src/app/layout.tsx 	new file:   src/components/Icons.tsx 	new file:   src/components/Navbar.tsx 	new file:   src/components/SignIn.tsx 	new file:   src/components/UserAccountNav.tsx 	new file:   src/components/UserAuthForm.tsx 	new file:   src/components/UserAvatar.tsx 	new file:   src/components/ui/Avatar.tsx 	new file:   src/components/ui/DropdownMenu.tsx 	new file:   src/components/ui/Toast.tsx 	new file:   src/components/ui/Toaster.tsx 	new file:   src/hooks/use-toast.ts 	new file:   src/lib/auth.ts 	new file:   src/types/next-auth.d.ts 	deleted:    yarn.lock

---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { FC, useState } from "react";
+import { Button } from "./ui/Button";
+import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { Icons } from "./Icons";
+import { useToast } from "@/hooks/use-toast";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn("github");
+    } catch (error) {
+      toast({
+        title: "There was a problem",
+        description: "There was an error loggin in with Google",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className={cn("flex justify-center", className)} {...props}>
+      <Button
+        isLoading={isLoading}
+        type="button"
+        size="sm"
+        className="w-full"
+        onClick={loginWithGoogle}
+        disabled={isLoading}
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
+        Google
+      </Button>
+    </div>
+  );
+};
+
+export default UserAuthForm;
